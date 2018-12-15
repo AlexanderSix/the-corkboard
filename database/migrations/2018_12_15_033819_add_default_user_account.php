405b3d59d2f29c5e88eb55d1361c194b54105fec
Alexander Six <alexanderhsix@gmail.com>
2018-12-14 22:43:47 -0500

    Adds a migration to create a default user account

---
+++ database/migrations/2018_12_15_033819_add_default_user_account.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultUserAccount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $user = new \App\User();
+        $user->name = env("DEFAULT_USER_NAME", "Default Name");
+        $user->email = env("DEFAULT_EMAIL", "email@example.com");
+        $user->email_verified_at = now();
+        $user->password = Hash::make(env("DEFAULT_PASSWORD", "password"));
+
+        $user->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $user = \App\User::where('email', env("DEFAULT_EMAIL", "email@example.com"))->first();
+
+        $user->delete();
+    }
+}
