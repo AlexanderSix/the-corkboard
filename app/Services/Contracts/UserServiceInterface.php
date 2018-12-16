1e63f1bd431a865056a2ca7abaa64831d4139610
Alexander Six <alexanderhsix@gmail.com>
2018-12-16 14:45:44 -0500

    Adds User service layer files--replaces the Repository pattern files from last commit (we aren't ever going to switch from using Eloquent, so abstracting the business logic to this layer seems like a better choice)

---
+++ app/Services/Contracts/UserServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexandersix
+ * Date: 12/16/18
+ * Time: 2:41 PM
+ */
+
+namespace App\Services\Contracts;
+
+
+interface UserServiceInterface
+{
+
+}
---
+++ app/Services/UserServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'App\Services\Contracts\UserServiceInterface',
+            'App\Services\UserServiceProvider'
+        );
+    }
+}
