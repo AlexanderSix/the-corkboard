7a4e793809809dedc0a23ea39dda92433e2905ab
Alexander Six <alexanderhsix@gmail.com>
2018-12-16 12:56:31 -0500

    Adds a basic MessageFactory for DB seeding & testing

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -2,8 +2,17 @@
 
 use Faker\Generator as Faker;
 
+//$user = factory(App\User::class)->make();
+//
 $factory->define(App\Message::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => (factory(App\User::class)->create())->id,
+        'title' => $faker->sentence(),
+        'message' => $faker->realText()
     ];
 });
+
+// TODO: Figure out why save() isn't a method on this class.
+//$factory->afterCreating(App\Message::class, function (App\Message $message, Faker $faker) {
+//    $message->user()->save(factory(App\User::class)->make());
+//});
