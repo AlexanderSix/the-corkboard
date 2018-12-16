925851eab305afe5a23a29135b53500ce14798b9
Alexander Six <alexanderhsix@gmail.com>
2018-12-16 13:05:05 -0500

    Sets up a basic ResponseFactory for seeding and testing

--- database/factories/ResponseFactory.php
+++ database/factories/ResponseFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Response::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => (factory(App\User::class)->create())->id,
+        'message_id' => (factory(App\Message::class)->create())->id,
+        'message' => $faker->text()
     ];
 });
