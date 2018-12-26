8f1aa1530ce0e352672fa6b277f317beff0f2149
Alexander Six <alexanderhsix@gmail.com>
2018-12-26 15:23:55 -0500

    More styling and moving to a component!

---
+++ resources/js/components/RegisterCard.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <p>Register</p>
+        </div>
+
+        <div class="card-content">
+            <form :action="submitRoute" method="POST">
+                <div class="field">
+                    <label for="name" class="label">Name</label>
+                    <div class="control">
+                        <input id="name"
+                               type="text"
+                               class="input"
+                               <!--:class="('name' in errors) ? ' is-danger' : ''"-->
+                               placeholder="Name"
+                               name="name"
+                               :value="oldValues.name"
+                               required autofocus
+                        >
+                        {{ errors }}
+                        <!--<span v-if="hasError('name')" class="invalid-feedback" role="alert">-->
+                            <!--<strong>{{ getError('name') }}</strong>-->
+                        <!--</span>-->
+                    </div>
+                </div>
+
+                <!--<div class="field">-->
+                    <!--<label for="email" class="label">Email</label>-->
+                    <!--<div class="control">-->
+                        <!--<input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" placeholder="Email" name="email" value="{{ old('email') }}" required>-->
+
+                        <!--@if ($errors->has('email'))-->
+                        <!--<span class="invalid-feedback" role="alert">-->
+                                                <!--<strong>{{ $errors->first('email') }}</strong>-->
+                                            <!--</span>-->
+                        <!--@endif-->
+                    <!--</div>-->
+                <!--</div>-->
+
+                <!--<div class="field">-->
+                    <!--<label for="password" class="label">Password</label>-->
+                    <!--<div class="control">-->
+                        <!--<input id="password" type="password" class="input{{ $errors->has('password') ? ' is-danger' : '' }}" placeholder="Password" name="password" required>-->
+
+                        <!--@if ($errors->has('password'))-->
+                        <!--<span class="invalid-feedback" role="alert">-->
+                                                <!--<strong>{{ $errors->first('password') }}</strong>-->
+                                            <!--</span>-->
+                        <!--@endif-->
+                    <!--</div>-->
+                <!--</div>-->
+
+                <!--<div class="field">-->
+                    <!--<label for="password-confirm" class="label">Confirm Password</label>-->
+                    <!--<div class="control">-->
+                        <!--<input id="password-confirm" type="password" class="input{{ $errors->has('password-confirm') ? ' is-danger' : '' }}" placeholder="Confirm Password" name="password-confirm" required>-->
+                    <!--</div>-->
+                <!--</div>-->
+
+
+                <!--<div class="field">-->
+                    <!--<div class="control">-->
+                        <!--<button class="button is-link" type="submit">Register</button>-->
+                    <!--</div>-->
+                <!--</div>-->
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RegisterCard",
+        props: ['errors', 'submitRoute', 'oldValues'],
+        computed: {
+            // hasError(key) {
+            //     return key in errors
+            // }
+        },
+        methods: {
+            getError(key) {
+                return errors.key
+            }
+        },
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
 
-    <div class="section">
+    <div class="section register-section">
         <div class="container">
             <div class="columns">
-                <div class="column is-8 is-offset-2">
-                    <div class="card">
+                <div class="column">
+                    <register-card
+                        errors="{{ $errors }}"
+
+                    />
+                </div>
+
+                <div class="column">
+                    <div class="card register-card">
                         <div class="card-header">
                             <header class="card-header-title">
                                 <p>{{ __('Register') }}</p>
@@ -18,7 +25,7 @@
                                 <div class="field">
                                     <label for="name" class="label">{{ __('Name') }}</label>
                                     <div class="control">
-                                        <input type="text" class="input{{ $errors->has('name') ? ' is-danger' : '' }}" placeholder="Name" name="name" value="{{ old('name') }}" required autofocus>
+                                        <input id="name" type="text" class="input{{ $errors->has('name') ? ' is-danger' : '' }}" placeholder="Name" name="name" value="{{ old('name') }}" required autofocus>
 
                                         @if ($errors->has('name'))
                                             <span class="invalid-feedback" role="alert">
@@ -31,21 +38,33 @@
                                 <div class="field">
                                     <label for="email" class="label">{{ __('E-Mail Address') }}</label>
                                     <div class="control">
+                                        <input id="email" type="email" class="input{{ $errors->has('email') ? ' is-danger' : '' }}" placeholder="Email" name="email" value="{{ old('email') }}" required>
 
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
                                     </div>
                                 </div>
 
                                 <div class="field">
                                     <label for="password" class="label">{{ __('Password') }}</label>
                                     <div class="control">
+                                        <input id="password" type="password" class="input{{ $errors->has('password') ? ' is-danger' : '' }}" placeholder="Password" name="password" required>
 
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
                                     </div>
                                 </div>
 
                                 <div class="field">
                                     <label for="password-confirm" class="label">{{ __('Confirm Password') }}</label>
                                     <div class="control">
-
+                                        <input id="password-confirm" type="password" class="input{{ $errors->has('password-confirm') ? ' is-danger' : '' }}" placeholder="Confirm Password" name="password-confirm" required>
                                     </div>
                                 </div>
 
@@ -63,78 +82,10 @@
             </div>
         </div>
     </div>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
+{{--<script>--}}
+    {{--import RegisterCard from "../../js/components/RegisterCard";--}}
+    {{--export default {--}}
+        {{--components: {RegisterCard}--}}
+    {{--}--}}
+{{--</script>--}}
