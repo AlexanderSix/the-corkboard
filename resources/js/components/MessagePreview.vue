03c07f8a432e3ac494adc720fec09922e76d0a43
Alexander Six <alexanderhsix@gmail.com>
2018-12-29 07:25:02 -0600

    Adds a Message Preview component

---
+++ resources/js/components/MessagePreview.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="box">
+        <article class="media">
+            <div class="media-left">
+                <figure class="image is-64">
+                    <img :src="imageSource" alt="Profile Image" class="profile-image" />
+                </figure>
+            </div>
+            <div class="media-content">
+                <h4 class="message-content-subject">
+                    {{ messageTitle }}
+                </h4>
+                <hr>
+                <p class="message-content">
+                    {{ messageBody }}
+                </p>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MessagePreview",
+        props: ['imageSource', 'messageTitle', 'messageBody'],
+        computed: {
+
+        },
+        methods: {
+
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '../../sass/variables';
+
+.profile-image {
+    border-radius: 64px;
+    border: 1px solid $gray-800;
+}
+
+.message-content-subject {
+    font-weight: 600;
+    text-align: left;
+}
+
+.message-content {
+    text-align: left;
+}
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    @foreach($messages as $message)
+                        {{-- Put a message component here --}}
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
